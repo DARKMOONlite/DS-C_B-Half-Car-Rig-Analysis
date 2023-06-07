729c802f4cc2b6ca134fb1e1c310613af62de7d1
funkybubble <gabrielleomullen@gmail.com>
2023-06-07 16:54:21 +1000

    ftt practise

--- test_file.m
+++ test_file.m
@@ -8,3 +8,25 @@ for i=1:size(data,2)
     plot(data(i).catagoriseddata.time,data(i).rawdata(:,2:5))
     title(strcat(data(i).Lift_Position,"\_",data(i).Test,"\_",data(i).damping,"\_",data(i).mass))
 end
+
+%% FFT
+
+
+fft_data = fft(data(1).rawdata(:,2:5),[],1)
+
+plot(data(1).catagoriseddata.time, fft_data)
+
+%%%Compute the single-sided amplitude spectrum of the signal.
+fft_data = fft(data(1).rawdata(:,2:5),[],1);
+Fs = 5000;
+L = 20000;
+f = Fs*(0:(L-1)/2)/L;
+P2 = abs(fft_data/L);
+P1 = P2(1:(L+1)/2);
+P1(2:end) = 2*P1(2:end);
+%%In the frequency domain, plot the single-sided spectrum. Because the time sampling of the signal is quite short, the frequency resolution of the Fourier transform is not precise enough to show the peak frequency near 4 Hz.
+
+plot(f,P1,"-o") 
+title("Single-Sided Spectrum of Original Signal")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
