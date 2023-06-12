0f039cd4051f8f75cdb9ff4f8ffcf7b84ff6e210
funkybubble <gabrielleomullen@gmail.com>
2023-06-12 16:46:39 +1000

    changes since pull (merge changes now added)

--- test_file.m
+++ test_file.m
@@ -46,6 +46,37 @@ for i=1:length(data)/2
     title(strcat(data(i).Lift_Position,"\_",data(i).Test,"\_",data(i).damping,"\_",data(i).mass));
 end
 
+%%gm FFT 
+% Extract Data
+%data = Extract_Half_Car_Rig_Data();
+
+Fs = 5000;  % sampling frequency
+
+% Create tiled layout
+tiledlayout(8,4)
+
+% Iterate over all data sets
+for i=1:size(data,2)
+    % Compute FFT of the data
+    fft_data = fft(data(i).rawdata(:,4));
+    L = length(data(i).rawdata(:,4));  % length of the signal
+    f = Fs*(0:(L/2))/L;  % frequency vector
+    P2 = abs(fft_data/L);  % double-sided spectrum
+    P1 = P2(1:L/2+1);  % single-sided spectrum
+    P1(2:end-1) = 2*P1(2:end-1);
+
+    % Plot the single-sided amplitude spectrum in the next tile
+    nexttile;
+    semilogy(f,P1)
+    title(strcat(data(i).Lift_Position,"\_",data(i).Test,"\_",data(i).damping,"\_",data(i).mass));
+    xlabel('Frequency (f)')
+    ylabel('|P1(f)| in dB')
+    
+    % Set the x-axis limit to 100
+    xlim([0 20])
+end
+
+
 % fft_data = fft(data(1).rawdata(:,2:5),[],1)
 % 
 % plot(data(1).cdata.time, fft_data)
